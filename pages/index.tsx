ec12d54cc1de3a806c80fea96bba0827837f7718
AFIFIHishame-Z <hishame.afifi1997@gmail.com>
2022-06-20 15:20:25 +0100

    [#3] :sparkles: : Building the product feed component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Banner from "../components/banner/Banner";
 import Header from "../components/header/Header";
+import ProductFeed from "../components/products/ProductFeed";
 
-const Home: NextPage = () => {
+const Home: NextPage<any> = ({ products }: { products: any }) => {
   return (
-    <main>
+    <main className="bg-gray-100">
       <Head>
         <title>Amazon store v2.0</title>
         <meta name="description" content="Rebuild Amazon store using NextJs" />
@@ -14,10 +15,27 @@ const Home: NextPage = () => {
         <Header />
       </div>
       <section className="max-w-screen-2xl mx-auto">
-        <Banner />
+        <article>
+          <Banner />
+        </article>
+        <article>
+          <ProductFeed products={products} />
+        </article>
       </section>
     </main>
   );
 };
 
 export default Home;
+
+export async function getServerSideProps(context: any) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products: products,
+    },
+  };
+}
